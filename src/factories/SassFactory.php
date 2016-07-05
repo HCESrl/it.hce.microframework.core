d98344d1f3f57ae20c5bb30ee6f4fbe15a3b6e32
Marco Boffo <mboffo@hce.it>
2016-07-05 14:33:38 +0200

    RTL addon

--- src/factories/SassFactory.php
+++ src/factories/SassFactory.php
@@ -5,6 +5,7 @@ use it\hce\microframework\core\MicroFramework;
 use it\hce\microframework\core\exceptions\ResourceWriteException;
 use Leafo\ScssPhp\Compiler;
 use MatthiasMullie\Minify\CSS;
+use CSSJanus;
 
 class SassFactory {
     private $compiler;
@@ -32,9 +33,16 @@ class SassFactory {
 
     public function write($file)
     {
-        $this->minifier->add($this->compiler->compile($this->main));
+        $compiledSass = $this->compiler->compile($this->main);
 
         if(is_writable(dirname($file))) {
+            if(isset($_GET['rtl']) && $_GET['rtl'] === 'true') {
+                include_once(MicroFramework::getBasePath() . 'vendor/cssjanus/cssjanus/src/CSSJanus.php');
+                $this->minifier->add(CSSJanus::transform($compiledSass));
+            } else {
+                $this->minifier->add($compiledSass);
+            }
+
             file_put_contents($file, $this->minifier->minify());
         } else {
             throw new ResourceWriteException($file . ' not writable');
